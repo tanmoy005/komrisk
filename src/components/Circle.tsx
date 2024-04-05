d9c6ba24477ccfc61cbcc98ebcc637878faaafe9
elogixDibyo <dibyojit@elogixmail.com>
2024-04-05 19:39:02 +0530

    created task card

---
+++ src/components/Circle.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View } from 'react-native'
+import { styles } from '../style'
+
+const Circle = () => {
+  return (
+    <View style={styles.circle}></View>
+  )
+}
+
+export default Circle
