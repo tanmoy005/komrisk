28cbf408d6c5d62861f73efd6a786aa7d260f607
elogixDibyo <dibyojit@elogixmail.com>
2024-04-05 19:34:48 +0530

    input field created

---
+++ src/components/input-fields/InputField.tsx
@@ -0,0 +1,44 @@
+import { InputFieldProps } from '@/src/types';
+import React from 'react'
+import { StyleSheet, TextInput } from 'react-native'
+
+
+const InputField = ({ value, setInput, placeholder, type}: InputFieldProps) => {
+
+    const type1 = {
+        ...styles.input,...styles.inputType1
+    }
+console.log('type', type);
+
+
+    return (
+        <TextInput
+            style={{...styles.input, ...styles['inputType'+type]}}
+            onChangeText={(value: string) => setInput(value)}
+            value={value}
+            placeholder={placeholder}
+        />
+    )
+}
+const styles: { [key: string]: any } = StyleSheet.create({
+
+    input: {
+        borderColor: '#D9D9D9',
+        borderWidth: 2,
+        borderRadius: 5,
+        width: '100%'
+    },
+    inputType1:{
+        // padding: 13,
+        height: 33,
+        color: '#99A3A4',
+        
+    },
+    inputType2:{
+        paddingHorizontal: 14,
+        paddingVertical: 7,
+        color: '#B6B6B9',
+        backgroundColor: '#fff'
+    }
+});
+export default InputField
