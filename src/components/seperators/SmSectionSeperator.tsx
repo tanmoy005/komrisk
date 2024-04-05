95bb7724b8edad518952521b45f174d5227cd840
elogixDibyo <dibyojit@elogixmail.com>
2024-04-05 19:42:40 +0530

    created section seperator

---
+++ src/components/seperators/SmSectionSeperator.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Divider } from 'react-native-elements'
+
+const SmSectionSeperator = () => <Divider color ='transparent' style={{height: 5}} /> 
+
+export default SmSectionSeperator
