edb4a261f3a208f77a3ad044ba1a29e16775a7fe
elogixDibyo <dibyojit@elogixmail.com>
2024-04-10 11:55:07 +0530

    type changed from jsx to tsx

--- src/utils/associate/has-value.tsx
+++ src/utils/associate/has-value.tsx
@@ -1,4 +1,4 @@
-    export const hasValue = (element) => {
+    export const hasValue = (element: any): boolean => {
 
     if (element === "" || element === null || element === undefined || /^\s*$/.test(element)) {
         return false;
