20b21134a8f44263e08fcd0af6cc0fd63d8e20ab
tanmoy@elogixmail.com <tanmoy@elogixmail.com>
2024-04-10 15:44:54 +0530

    percentage component added

--- src/utils/associate/get-percentage.tsx
+++ src/utils/associate/get-percentage.tsx
@@ -1,5 +1,5 @@
-const calculatePercentage = (part: number, total: number): number => {
-    if (total === 0) return 0; // Handle division by zero
+const calculatePercentage = (part: number, total: number): string => {
+    if (total === 0) return '0'; // Handle division by zero
     return ((part / total) * 100).toPrecision(2);
 };
 export default calculatePercentage;
