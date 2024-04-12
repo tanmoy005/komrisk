7b6d297d4b6d7bdd205fc2706e8cc1867632ade5
elogixDibyo <dibyojit@elogixmail.com>
2024-04-12 12:39:57 +0530

    Created task card

--- src/components/cards/TaskCard.tsx
+++ src/components/cards/TaskCard.tsx
@@ -21,71 +21,71 @@ const TaskCard = ({ taskCard }: taskCardDataProp) => {
         <View style={styles.taskCard}>
             <View>
 
-            <CardContainer>
-                <CardTextContainer>
-                    {
-                        firstSection &&
-                        firstSection.heading && <SmallHeading>{firstSection.heading}</SmallHeading>
-                    }
-                    {
-                        firstSection &&
-                        firstSection.description &&
-                        <Text style={styles.shortDescription}>{firstSection.description?.trim()}</Text>
-                    }
-                    <SmSectionSeperator />
-                    {
-                        secondSection &&
-                        secondSection.heading &&
-                        <SmallHeading>{secondSection.heading}</SmallHeading>
-                    }
-                    {
-                        secondSection && secondSection.description &&
-                        <Text style={styles.shortDescription}>{secondSection.description?.trim()}</Text>
-                    }
-                </CardTextContainer>
-            </CardContainer>
-            <CardContainer styles={{
-                backgroundColor: 'rgba(120, 106, 205, 0.08)'
-            }}>
-                <CardTextContainer>
-
-                <View style={{
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
-                    alignItems: 'center'
-                    // columnGap: 24
+                <CardContainer>
+                    <CardTextContainer>
+                        {
+                            firstSection &&
+                            firstSection.heading && <SmallHeading>{firstSection.heading}</SmallHeading>
+                        }
+                        {
+                            firstSection &&
+                            firstSection.description &&
+                            <Text style={styles.shortDescription}>{firstSection.description?.trim()}</Text>
+                        }
+                        <SmSectionSeperator />
+                        {
+                            secondSection &&
+                            secondSection.heading &&
+                            <SmallHeading>{secondSection.heading}</SmallHeading>
+                        }
+                        {
+                            secondSection && secondSection.description &&
+                            <Text style={styles.shortDescription}>{secondSection.description?.trim()}</Text>
+                        }
+                    </CardTextContainer>
+                </CardContainer>
+                <CardContainer styles={{
+                    backgroundColor: 'rgba(120, 106, 205, 0.08)'
                 }}>
-                    <View style={{ width: 120 }}>
-                        {
-                            thirdSection.dateHeading &&
-                            <SmallHeading>{thirdSection.dateHeading}</SmallHeading>
-                        }
-                        {
-                            thirdSection.date &&
-                            <Text style={styles.bodyInfoText}>{thirdSection.date}</Text>
-                        }
-                    </View>
-                    <View style={{
-                        flexDirection: 'row',
-                        columnGap: 14
-                    }}>
-                        {
-                            thirdSection.sectionRight && thirdSection.sectionRight.map(({ taskDesg, name, pic }, index) => {
-                                return (
-                                    <View key={index} style={styles.taskCardBottomRightSection}>
-                                        <SmallHeading>{taskDesg}</SmallHeading>
-                                        {pic ? pic :
-                                            <Circle />
-                                        }
-                                        <Text style={styles.bodyInfoText}>{name}</Text>
-                                    </View>
-                                )
-                            })
-                        }
-                    </View>
-                </View>
-                </CardTextContainer>
-            </CardContainer>
+                    <CardTextContainer>
+
+                        <View style={{
+                            flexDirection: 'row',
+                            justifyContent: 'space-between',
+                            alignItems: 'center'
+                            // columnGap: 24
+                        }}>
+                            <View style={{ width: 120 }}>
+                                {
+                                    thirdSection.dateHeading &&
+                                    <SmallHeading>{thirdSection.dateHeading}</SmallHeading>
+                                }
+                                {
+                                    thirdSection.date &&
+                                    <Text style={styles.bodyInfoText}>{thirdSection.date}</Text>
+                                }
+                            </View>
+                            <View style={{
+                                flexDirection: 'row',
+                                columnGap: 14
+                            }}>
+                                {
+                                    thirdSection.sectionRight && thirdSection.sectionRight.map(({ taskDesg, name, pic }, index) => {
+                                        return (
+                                            <View key={index} style={styles.taskCardBottomRightSection}>
+                                                <SmallHeading>{taskDesg}</SmallHeading>
+                                                {pic ? pic :
+                                                    <Circle />
+                                                }
+                                                <Text style={styles.bodyInfoText}>{name}</Text>
+                                            </View>
+                                        )
+                                    })
+                                }
+                            </View>
+                        </View>
+                    </CardTextContainer>
+                </CardContainer>
             </View>
         </View>
     )
