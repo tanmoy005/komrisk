3f036003c2fb4bc7faa6c52a8d315d4d6f849e8e
elogixDibyo <dibyojit@elogixmail.com>
2024-04-12 11:45:10 +0530

    name correction

--- src/app/(user)/chartReport/_layout.tsx
+++ src/app/(user)/chartReport/_layout.tsx
@@ -5,7 +5,7 @@ const ChartReportLayout = () => {
   return (
     <Stack>
       <Stack.Screen name="GetActivityStatusDataListDetailsInfo" options={{ title: 'Activity Task List', headerShown: true }} />
-      <Stack.Screen name="GetComplianceStatusDataListDetailsInfo" options={{ title: 'Compiance Task List', headerShown: true }} />
+      <Stack.Screen name="GetComplianceStatusDataListDetailsInfo" options={{ title: 'Compliance Task List', headerShown: true }} />
       <Stack.Screen name="GetIncidentActivityDataListDetailsInfo" options={{ title: 'Incident Activity List', headerShown: true }} />
       <Stack.Screen name="GetImpactAnalysisDataListDetailsInfo" options={{ title: 'Impact Analysis List', headerShown: true }} />
       <Stack.Screen name="GetIncidentComparisonDataListDetailsInfo" options={{ title: 'Incident Comparison List', headerShown: true }} />
