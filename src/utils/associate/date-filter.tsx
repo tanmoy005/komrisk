1786aa701dda89d24623bd68750c61138706deff
elogixDibyo <dibyojit@elogixmail.com>
2024-04-16 13:13:01 +0530

    string to date added

--- src/utils/associate/date-filter.tsx
+++ src/utils/associate/date-filter.tsx
@@ -1,9 +1,17 @@
 import moment from "moment/moment";
+import { hasValue } from "./has-value";
 
 
 export const DateFormatDDMMYYYY = (dateTime: string | undefined | null) => {
   // console.log('dateTime', dateTime);
-  
+
   return dateTime && moment(dateTime).format("DD/MM/YYYY");
+};
+
+export const StringToDate = (dateTime: string) => {
+  // console.log('dateTime', dateTime);
+  let timestamp = Date.parse(dateTime);
+  const dateObj = new Date(timestamp);
+  return dateObj;
 };
 
