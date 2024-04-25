27e72ccc078a713f47854eaa433d4c3a15bdc9f8
tanmoy@elogixmail.com <tanmoy@elogixmail.com>
2024-04-25 12:22:18 +0530

    Task list file added

--- assets/data/chartdataList.ts
+++ assets/data/chartdataList.ts
@@ -13,11 +13,30 @@ const ComplianceChartDataList = {
             "owner": "Aditya Chaudhuri",
             "currOwner": null,
             "reviewer": "Aditya Gmail",
+            "Assignee": "Aditya Gmail",
             "dueDate": "31/12/2023",
             "impact": "Medium",
             "status": 74957,
             "taskId": 6490045,
-            "complianceGenId": "IQS-AMD000100011"
+            "complianceGenId": "IQS-AMD000100011",
+            "activities": [
+                {
+                    "type": "update law data",
+                    "updateOn": "31/05/2023",
+                    "updatedBy": "test Usir 1"
+                },
+                {
+                    "type": "update tsjs law data",
+                    "updateOn": "31/08/2023",
+                    "updatedBy": "test Usir 1"
+                },
+                {
+                    "type": "AAirlaw data",
+                    "updateOn": "31/11/2023",
+                    "updatedBy": "test Usir 1"
+                }
+
+            ]
         },
         {
             "mapId": 75779,
@@ -30,12 +49,31 @@ const ComplianceChartDataList = {
             "opUnit": "Bangalore devopment Centre",
             "owner": "Aditya Chaudhuri",
             "currOwner": "Aditya Chaudhuri",
+            "Assignee": "Aditya Gmail",
             "reviewer": "User Q",
             "dueDate": "29/12/2023",
             "impact": "null",
             "status": 75591,
             "taskId": 75591,
-            "complianceGenId": "AUTOC000100002"
+            "complianceGenId": "AUTOC000100002",
+            "activities": [
+                {
+                    "type": "aleim;a law data",
+                    "updateOn": "31/05/2023",
+                    "updatedBy": "tesasdadt Usir 1"
+                },
+                {
+                    "type": "update tsjs law data",
+                    "updateOn": "31/08/2023",
+                    "updatedBy": "test Usir 1"
+                },
+                {
+                    "type": "AAirlaw data",
+                    "updateOn": "31/11/2023",
+                    "updatedBy": "test Usir 1"
+                }
+
+            ]
         },
         {
             "mapId": 75778,
@@ -49,11 +87,26 @@ const ComplianceChartDataList = {
             "owner": "Aditya Chaudhuri",
             "currOwner": "Aditya Chaudhuri",
             "reviewer": "User Q",
+            "Assignee": "Aditya Gmail",
             "dueDate": "22/12/2023",
             "impact": "null",
             "status": 75590,
             "taskId": 75590,
-            "complianceGenId": "AUTOC000100002"
+            "complianceGenId": "AUTOC000100002",
+            "activities": [
+                {
+                    "type": "adadad law data",
+                    "updateOn": "31/05/2023",
+                    "updatedBy": "ngdkhterwst Usir 1"
+                },
+
+                {
+                    "type": "AAirlaw dfeecataada",
+                    "updateOn": "31/11/2023",
+                    "updatedBy": "teasdarst Usir 1"
+                }
+
+            ]
         },
         {
             "mapId": 75777,
@@ -67,11 +120,26 @@ const ComplianceChartDataList = {
             "owner": "Aditya Chaudhuri",
             "currOwner": "Aditya Chaudhuri",
             "reviewer": "User Q",
+            "Assignee": "Aditya Gmail",
             "dueDate": "15/12/2023",
             "impact": "null",
             "status": 75589,
             "taskId": 75589,
-            "complianceGenId": "AUTOC000100002"
+            "complianceGenId": "AUTOC000100002",
+            "activities": [
+                {
+                    "type": "adadad law data",
+                    "updateOn": "31/05/2023",
+                    "updatedBy": "ngdkhterwst Usir 1"
+                },
+
+                {
+                    "type": "AAirlaw dfeecataada",
+                    "updateOn": "31/11/2023",
+                    "updatedBy": "teasdarst Usir 1"
+                }
+
+            ]
         },
         {
             "mapId": 75776,
@@ -85,11 +153,26 @@ const ComplianceChartDataList = {
             "owner": "Aditya Chaudhuri",
             "currOwner": "Aditya Chaudhuri",
             "reviewer": "User Q",
+            "Assignee": "Aditya Gmail",
             "dueDate": "08/12/2023",
             "impact": "null",
             "status": 75588,
             "taskId": 75588,
-            "complianceGenId": "AUTOC000100002"
+            "complianceGenId": "AUTOC000100002",
+            "activities": [
+                {
+                    "type": "adadad law data",
+                    "updateOn": "31/05/2023",
+                    "updatedBy": "ngdkhterwst Usir 1"
+                },
+
+                {
+                    "type": "AAirlaw dfeecataada",
+                    "updateOn": "31/11/2023",
+                    "updatedBy": "teasdarst Usir 1"
+                }
+
+            ]
         },
         {
             "mapId": 75775,
@@ -103,11 +186,13 @@ const ComplianceChartDataList = {
             "owner": "Aditya Chaudhuri",
             "currOwner": "Aditya Chaudhuri",
             "reviewer": "User Q",
+            "Assignee": "Aditya Gmail",
             "dueDate": "01/12/2023",
             "impact": "null",
             "status": 75587,
             "taskId": 75587,
-            "complianceGenId": "AUTOC000100002"
+            "complianceGenId": "AUTOC000100002",
+            "activities": []
         }
     ],
     "iTotalRecords": 6,
