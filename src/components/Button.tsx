493b002cf73a5e9a190f382978e410272d331db8
tanmoy@elogixmail.com <tanmoy@elogixmail.com>
2024-04-26 15:32:01 +0530

    button touchable opacity add and haptic feedback added

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
-import { Pressable, StyleProp, Text, TextStyle, View } from 'react-native';
-import { forwardRef } from 'react';
+import React, { forwardRef } from 'react';
+import { TouchableOpacity, Text, View, TextStyle, StyleProp } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-
+import * as Haptics from 'expo-haptics';
 
 type ButtonProps = {
   text?: string;
@@ -20,98 +20,52 @@ type ButtonProps = {
   };
   type?: string;
   btnColor?: string;
-} & React.ComponentPropsWithoutRef<typeof Pressable>;
+  onPress?: () => void;
+} & React.ComponentPropsWithoutRef<typeof TouchableOpacity>;
 
-
-const Button = forwardRef<View | null, ButtonProps>(
-  ({ text, btnColor, icon, leftIcon, style, type = 'default', ...pressableProps }, ref) => {
-
+const Button = forwardRef<TouchableOpacity, ButtonProps>(
+  ({ text, btnColor, icon, leftIcon, style, type = 'default', onPress, ...touchableOpacityProps }, ref) => {
     const backgroundColor = (type === 'default' || type === 'md-default' || type === 'btnIcon' || type === 'sm') ? btnColor : 'transparent';
-    let _color;
-    // console.log('style?.color', style?.color);
-
-    if (type === 'default' || type === 'md-default' || type === 'btnIcon') {
-      _color = '#fff';
-    } else if (style?.color) {
+    let _color = '#fff';
+    if (type !== 'default' && type !== 'md-default' && type !== 'btnIcon' && style?.color) {
       _color = style.color;
-    } else {
-      _color = btnColor;
+    } else if (type !== 'default' && type !== 'md-default' && type !== 'btnIcon') {
+      _color = btnColor?? 'trans';
     }
-    // console.log('_color', _color);
 
-    // const _color = (type === 'default' || 'btnIcon') ? '#fff' : btnColor;
     const btnTextStyle: StyleProp<TextStyle> = {
       color: _color,
       fontSize: style?.fontSize,
       fontWeight: '400',
       textAlign: 'center'
-    }
+    };
 
-    let paddingHorizontal;
-    let paddingVertical;
-    let fontSize;
-    let fontWeight;
-    let borderRadius;
-    let padding;
-    let width;
-    if (type === 'sm') {
-      paddingVertical = 5;
-      paddingHorizontal = 10;
-      fontWeight = '400';
-      fontSize = 12;
-      borderRadius = 5;
-    } else if (type === 'md-default') {
-      padding = 10.5;
-      fontWeight = '400';
-      fontSize = 16;
-      borderRadius = 5;
-      width = 104;
-    } else {
-      paddingVertical = style?.paddingVertical;
-      paddingHorizontal = style?.paddingHorizontal;
-      fontWeight = style?.fontWeight;
-      fontSize = style?.fontSize;
-      borderRadius = style?.borderRadius;
-      padding = style?.padding;
-      width = style?.width;
-    }
+    const handlePress = () => {
+      Haptics.selectionAsync(); // Triggers a light haptic effect
+      if (onPress) onPress(); // Call the passed onPress function if provided
+    };
 
     return (
-      <Pressable ref={ref} {...pressableProps} >
+      <TouchableOpacity ref={ref} onPress={handlePress} {...touchableOpacityProps}>
         <View style={{
-          paddingVertical: paddingVertical,
-          paddingHorizontal: paddingHorizontal,
-          padding: padding,
-          backgroundColor: backgroundColor,
-          borderWidth: 2,
-          borderStyle: 'solid',
-          borderColor: btnColor,
-          borderRadius: borderRadius,
-          width: style?.width,
-          height: style?.height,
           flexDirection: 'row',
           alignItems: 'center',
-          justifyContent: 'center'
+          justifyContent: 'center',
+          backgroundColor: backgroundColor,
+          borderRadius: style?.borderRadius,
+          borderColor: btnColor,
+          borderWidth: 2,
+          padding: style?.padding,
+          paddingHorizontal: style?.paddingHorizontal,
+          paddingVertical: style?.paddingVertical,
+          width: style?.width,
+          height: style?.height
         }}>
-          {
-            leftIcon &&
-            <Icon name={leftIcon}
-              size={24} color={_color}
-            />
-          }
-          {
-            text &&
-            <Text style={btnTextStyle}>{text}</Text>
-          }
-          {
-            icon &&
-            <Icon name={icon}
-              size={style?.fontSize} color={_color}
-              style={btnTextStyle}
-            />
-          }
+          {leftIcon && <Icon name={leftIcon} size={24} color={_color} />}
+          {text && <Text style={btnTextStyle}>{text}</Text>}
+          {icon && <Icon name={icon} size={style?.fontSize} color={_color} style={btnTextStyle} />}
         </View>
-      </Pressable>
+      </TouchableOpacity>
     );
   }
 );
