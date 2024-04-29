59664b12bfd226f8bdab3cc27d6278f603935c54
tanmoy@elogixmail.com <tanmoy@elogixmail.com>
2024-04-29 15:59:00 +0530

    selected button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import React, { forwardRef, useState } from 'react';
+import React, { forwardRef, useEffect, useState } from 'react';
 import { TouchableOpacity, Text, View, TextStyle, StyleProp, ViewStyle } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import * as Haptics from 'expo-haptics';
@@ -21,20 +21,35 @@ type ButtonProps = {
   type?: string;
   btnColor?: string;
   onPress?: () => void;
+  selected?: boolean;
 } & React.ComponentPropsWithoutRef<typeof TouchableOpacity>;
 
 const Button = forwardRef<TouchableOpacity, ButtonProps>(
-  ({ text, btnColor, icon, leftIcon, style, type = 'default', onPress, ...touchableOpacityProps }, ref) => {
-    const [isPressed, setIsPressed] = useState(false);
-
+  ({ text, btnColor, icon, leftIcon, style, type = 'default', onPress, selected, ...touchableOpacityProps }, ref) => {
+    const [isPressed, setIsPressed] = useState<boolean>(false);
+    const [isSelected, setIsSelected] = useState<boolean>(selected ?? false);
     const defaultButtonColor = type === 'default' || type === 'md-default' || type === 'btnIcon' || type === 'sm' ? btnColor : 'transparent';
     const textColor = type !== 'default' && type !== 'md-default' && type !== 'btnIcon' && style?.color ? style.color : type == 'outline' ? btnColor : "#fff";
+
+    // State to handle dynamic color changes based on selected and pressed state
+    const [dynamicBackgroundColor, setDynamicBackgroundColor] = useState(btnColor);
+
+    // Effect to update the background color based on isSelected and isPressed
+    useEffect(() => {
+      if (isSelected) {
+        setDynamicBackgroundColor('rgba(229, 227, 245, 1)');  // Selected state color
+      } else if (isPressed) {
+        setDynamicBackgroundColor('rgba(180, 180, 250, 1)'); // Pressed state color
+      } else {
+        setDynamicBackgroundColor(defaultButtonColor);  // Default color
+      }
+    }, [isSelected, isPressed, defaultButtonColor]);
 
     const buttonStyle: StyleProp<ViewStyle> = {
       flexDirection: 'row',
       alignItems: 'center',
       justifyContent: 'center',
-      backgroundColor: isPressed ? 'rgba(229, 227, 245, 1)' : defaultButtonColor,
+      backgroundColor: dynamicBackgroundColor,
       borderRadius: style?.borderRadius || 5,
       borderColor: btnColor,
       borderWidth: 2,
@@ -44,7 +59,7 @@ const Button = forwardRef<TouchableOpacity, ButtonProps>(
       width: style?.width,
       height: style?.height
     };
- console.log("================================Testcolor",textColor)
+    console.log("================================Testcolor", textColor)
     const textStyle: StyleProp<TextStyle> = {
       color: textColor,
       fontSize: style?.fontSize || 16,
@@ -67,6 +82,7 @@ const Button = forwardRef<TouchableOpacity, ButtonProps>(
         ref={ref}
         onPressIn={handlePressIn}
         onPressOut={handlePressOut}
+        onPress={onPress}
         style={[buttonStyle, isPressed ? { backgroundColor: 'rgba(229, 227, 245, 1)' } : {}]}
         {...touchableOpacityProps}
       >
