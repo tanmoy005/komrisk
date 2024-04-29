d944c84288947aef8b1c58e5814c371a7d5e03fb
tanmoy@elogixmail.com <tanmoy@elogixmail.com>
2024-04-29 15:46:43 +0530

    button on pren color change

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,5 @@
-import React, { forwardRef, useEffect, useState } from 'react';
-import { TouchableOpacity, Text, View, TextStyle, StyleProp } from 'react-native';
+import React, { forwardRef, useState } from 'react';
+import { TouchableOpacity, Text, View, TextStyle, StyleProp, ViewStyle } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import * as Haptics from 'expo-haptics';
 
@@ -25,101 +25,54 @@ type ButtonProps = {
 
 const Button = forwardRef<TouchableOpacity, ButtonProps>(
   ({ text, btnColor, icon, leftIcon, style, type = 'default', onPress, ...touchableOpacityProps }, ref) => {
-    const backgroundColor = (type === 'default' || type === 'md-default' || type === 'btnIcon' || type === 'sm') ? btnColor : 'transparent';
+    const [isPressed, setIsPressed] = useState(false);
 
-    const defaultBttnTextColor = style?.color ?? '';
-    const [_color, set_color] = useState('#fff');
+    const defaultButtonColor = type === 'default' || type === 'md-default' || type === 'btnIcon' || type === 'sm' ? btnColor : 'transparent';
+    const textColor = type !== 'default' && type !== 'md-default' && type !== 'btnIcon' && style?.color ? style.color : type == 'outline' ? btnColor : "#fff";
 
-    useEffect(() => {
-
-      if (type !== 'default' && type !== 'md-default' && type !== 'btnIcon' && style?.color) {
-        set_color(style.color);
-      } else if (type !== 'default' && type !== 'md-default' && type !== 'btnIcon') {
-        set_color(btnColor ?? 'trans');
-      }
-    }, [])
-
-
-    const btnTextStyle: StyleProp<TextStyle> = {
-      color: _color,
-      fontSize: style?.fontSize,
-      fontWeight: '400',
+    const buttonStyle: StyleProp<ViewStyle> = {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: isPressed ? 'rgba(229, 227, 245, 1)' : defaultButtonColor,
+      borderRadius: style?.borderRadius || 5,
+      borderColor: btnColor,
+      borderWidth: 2,
+      padding: style?.padding || 10,
+      paddingHorizontal: style?.paddingHorizontal || 10,
+      paddingVertical: style?.paddingVertical || 5,
+      width: style?.width,
+      height: style?.height
+    };
+ console.log("================================Testcolor",textColor)
+    const textStyle: StyleProp<TextStyle> = {
+      color: textColor,
+      fontSize: style?.fontSize || 16,
+      fontWeight: style?.fontWeight ?? '400',
       textAlign: 'center'
     };
 
-    const handlePress = () => {
-      Haptics.selectionAsync(); // Triggers a light haptic effect
-      if (onPress) onPress(); // Call the passed onPress function if provided
-     
+    const handlePressIn = () => {
+      setIsPressed(true);
+      Haptics.selectionAsync(); // Trigger haptic feedback on press
     };
-    const handleOnPresssIn =()=>{
-      if (backgroundColor === '#A097DC') {
-        set_color('rgba(38, 38, 44, 1)');
-      }
-    }
-    const handleOnPresssOut =()=>{
-      set_color(defaultBttnTextColor);
-    }
-    let paddingHorizontal;
-    let paddingVertical;
-    let fontSize;
-    let fontWeight;
-    let borderRadius;
-    let padding;
-    let width;
-    if (type === 'sm') {
-      paddingVertical = 5;
-      paddingHorizontal = 10;
-      fontWeight = '400';
-      fontSize = 12;
-      borderRadius = 5;
-    } else if (type === 'md-default' || type === 'md-outline') {
-      padding = 10.5;
-      fontWeight = '400';
-      fontSize = 16;
-      borderRadius = 5;
-      width = 104;
-    } else {
-      paddingVertical = style?.paddingVertical;
-      paddingHorizontal = style?.paddingHorizontal;
-      fontWeight = style?.fontWeight;
-      fontSize = style?.fontSize;
-      borderRadius = style?.borderRadius;
-      padding = style?.padding;
-      width = style?.width;
-    }
 
-    let backgroundColorOnTouch;
-
-    if (backgroundColor === '#A097DC') {
-      backgroundColorOnTouch = 'rgba(229, 227, 245, 1)'
-    }
-
-    let btnTouchStyle = {
-      borderRadius: borderRadius,
-      backgroundColor: backgroundColorOnTouch
-    }
+    const handlePressOut = () => {
+      setIsPressed(false);
+      if (onPress) onPress();
+    };
 
     return (
-      <TouchableOpacity ref={ref} onPressIn={handleOnPresssIn} onPressOut={handleOnPresssOut} onPress={handlePress} style={btnTouchStyle} {...touchableOpacityProps}>
-        <View style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'center',
-          backgroundColor: backgroundColor,
-          borderRadius: borderRadius,
-          borderColor: btnColor,
-          borderWidth: 2,
-          padding: padding,
-          paddingHorizontal: paddingHorizontal,
-          paddingVertical: paddingVertical,
-          width: width,
-          height: style?.height
-        }}>
-          {leftIcon && <Icon name={leftIcon} size={24} color={_color} />}
-          {text && <Text style={btnTextStyle}>{text}</Text>}
-          {icon && <Icon name={icon} size={style?.fontSize} color={_color} style={btnTextStyle} />}
-        </View>
+      <TouchableOpacity
+        ref={ref}
+        onPressIn={handlePressIn}
+        onPressOut={handlePressOut}
+        style={[buttonStyle, isPressed ? { backgroundColor: 'rgba(229, 227, 245, 1)' } : {}]}
+        {...touchableOpacityProps}
+      >
+        {leftIcon && <Icon name={leftIcon} size={24} color={textColor} />}
+        {text && <Text style={textStyle}>{text}</Text>}
+        {icon && <Icon name={icon} size={style?.fontSize} color={textColor} />}
       </TouchableOpacity>
     );
   }
