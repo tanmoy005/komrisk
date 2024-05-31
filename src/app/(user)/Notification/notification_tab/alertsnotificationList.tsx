3570c27ce79a6d7592c9411c7d990da5035711d1
Srilekha Chowdhury <srilekhachowdhury199@gmail.com>
2024-05-31 12:49:31 +0530

    alert_notification

--- src/app/(user)/Notification/notification_tab/alertsnotificationList.tsx
+++ src/app/(user)/Notification/notification_tab/alertsnotificationList.tsx
@@ -1,36 +1,4 @@
-// import HeadImageSection from '@/src/components/headSection/HeadImageSection';
-// import Seperator24 from '@/src/components/seperators/Seperator24';
-// import BtnFilterHeader from '@/src/components/tabs/BtnFilterHeader';
-// import { styles } from '@/src/style';
-// import React from 'react';
-// import { SafeAreaView } from 'react-native';
-// import NotificationList from '@/src/components/notification/nofificationList';
 
-// const alertsnotificationList: React.FC = () => {
-
-//   const handleSeeAll = () => {
-
-//   }
-
-//   return (
-//     <SafeAreaView style={styles.dashboardContainer}>
-//       <HeadImageSection />
-//       <BtnFilterHeader
-//         firstBtnName='See All'
-//         fistBtnOnpress={handleSeeAll}
-//         //secondBtnName='Closed'
-//         secondBtnOnpress={handleSeeAll}
-//       />
-//       <Seperator24 />
-//       <NotificationList />
-//     </SafeAreaView>
-//   );
-// };
-
-// export default alertsnotificationList;
-
-
-//=================================== Updated on 30-05-2024 ====================================== //
 
 import React, { useState, useCallback } from 'react';
 import { SafeAreaView, ActivityIndicator, View } from 'react-native';
